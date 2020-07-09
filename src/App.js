c24ed1f77691a9519f0d991118b8c1f6d817d823
zyhou <richard.maxime9@gmail.com>
2020-07-09 15:11:17 +0200

    Bootstrap detail charaters with GOT API

--- src/App.js
+++ src/App.js
@@ -1,26 +1,83 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { atom, selector, useRecoilState, useRecoilValue } from "recoil";
+import "./tailwind.output.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+import logo from "./images/logo.svg";
+import arrowBottom from "./images/arrow-bottom.svg";
+
+const apiUrl = `https://api.got.show//api/show/characters`;
+
+const currentCharacterIndexState = atom({
+  key: "CurrentCharactersIndex",
+  default: 0,
+});
+
+const characterListQuery = selector({
+  key: "CharacterListQuery",
+  get: async () => {
+    try {
+      const response = await fetch(apiUrl);
+      return await response.json();
+    } catch (error) {
+      throw error;
+    }
+  },
+});
+
+const currentCharacterInfoQuery = selector({
+  key: "CurrentCharacterInfoQuery",
+  get: ({ get }) => get(characterListQuery)[get(currentCharacterIndexState)],
+});
+
+const CharacterDetail = () => {
+  const currentCharacter = useRecoilValue(currentCharacterInfoQuery);
+  const [currentCharactersIndex, setCurrentCharactersIndex] = useRecoilState(
+    currentCharacterIndexState
   );
-}
+
+  const onNextCharacter = () => {
+    // todo: improve next item (selector/selectorFamily?)
+    let nextCharacterIndex = currentCharactersIndex + 1;
+    setCurrentCharactersIndex(nextCharacterIndex);
+  };
+
+  return (
+    <React.Fragment>
+      <div className="text-white flex items-center justify-center flex-col lg:flex-row py-8">
+        <section className="px-4 py-2 m-2">
+          <h1 className="font-bold text-4xl">{currentCharacter.name}</h1>
+          <div>
+            <ul>
+              <li>Actor: {currentCharacter.actor}</li>
+              <li>House: {currentCharacter.house}</li>
+              <li>Status: {currentCharacter.alive ? "Alive" : "Dead"}</li>
+            </ul>
+          </div>
+        </section>
+        <section>
+          <img src={currentCharacter.image} alt={currentCharacter.name} />
+        </section>
+      </div>
+      <section className="flex items-center justify-center">
+        <button onClick={onNextCharacter}>
+          <img src={arrowBottom} alt="Next Character" className="h-20" />
+        </button>
+      </section>
+    </React.Fragment>
+  );
+};
+
+const App = () => {
+  return (
+    <React.Fragment>
+      <header className="flex items-center justify-center py-8">
+        <img src={logo} alt="Logo" />
+      </header>
+      <React.Suspense fallback={<div>Loading...</div>}>
+        <CharacterDetail />
+      </React.Suspense>
+    </React.Fragment>
+  );
+};
 
 export default App;
